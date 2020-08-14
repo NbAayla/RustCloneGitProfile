bba21bacf469294de2927d8ae8dc2091ffc56181
afetzer <aayla@aayla.dev>
2020-08-13 21:41:46 -0600

    Finished functionality

--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,81 @@ use git2::Repository;
 use std::process::exit;
 
 fn main() {
-    let mut verbose = false;
-    let mut platform = String::new();
-    let mut username = String::new();
-    let mut path = String::new();
-
     // Set up Clap
     let yaml_args = load_yaml!("arguments.yaml");
     let matches = App::from(yaml_args).get_matches();
 
-    println!("{}", platform);
+    let platform = matches.value_of("PLATFORM").unwrap();
+    let username = matches.value_of("USERNAME").unwrap();
+
+
+    // Redefine path if set in arguments
+    let mut clone_path = "./";
+    if matches.is_present("path") {
+        clone_path = matches.value_of("path").unwrap();
+    } else {
+        exit(0);
+    }
+
+    // Validate inputs
     assert!(platform == "github" || platform == "gitlab");  // Ensure a valid platform
 
+    // Define variables
     let mut url = String::new();
     let mut page:i8 = 1;
+
+    // Create Client
+    let app_user_agent = concat!(
+        env!("CARGO_PKG_NAME"),
+        "/",
+        env!("CARGO_PKG_VERSION")
+    );
+    let client = reqwest::blocking::Client::builder()
+        .user_agent(app_user_agent)
+        .build()
+        .unwrap();
+
+    // Clone profile
     loop {
         if platform == "github" {
             url = f!("https://api.github.com/users/{username}/repos?per_page=100&page={page}");
         } else if platform == "gitlab" {
             url = f!("https://gitlab.com/api/v4/users/{username}/projects?per_page=100&page={page}");
         }
+
         // Get data and parse into json
-        let body = reqwest::blocking::get(&url)
+        let body = client.get(&url)
+            .send()
             .unwrap()
             .text()
             .unwrap();
+
         let parsed = json::parse(&*body).unwrap();
-        println!("{}", parsed);
-        exit(0);
+        // Break if response is empty
+        if parsed.is_empty() {
+            break;
+        }
+
+        // Iterate through the results
+        for i in 0..parsed.len() - 1 {
+            // Get the clone url of the repository
+            let mut repo_url = "";
+            if platform == "github" {
+                repo_url = parsed[i]["clone_url"].as_str().unwrap();
+            } else if platform == "gitlab" {
+                repo_url = parsed[i]["http_url_to_repo"].as_str().unwrap();
+            }
+            let repo_name = parsed[i]["name"].as_str().unwrap();
+
+            // Clone the repository
+            println!("Cloning {}", repo_name);
+            let repo = match Repository::clone(repo_url, format!("{}/{}", clone_path, repo_name)) {
+                Ok(repo) => repo,
+                Err(E) => panic!("Failed to clone: {}", E),
+            };
+        }
         // Iterate to next page
         page += 1;
     }
-
-    let body = reqwest::blocking::get("https://afetzer.com/en.json")
-        .unwrap()
-        .text()
-        .unwrap();
-    println!("{}", body);
-    let parsed = json::parse(&*body).unwrap();
-    assert_eq!(parsed["Name"], "Aayla Fetzer");
+    println!("Escaped!");
 }
